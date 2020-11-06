ddeac5391044969063e53725b7f470ac6b18a818
Jawahar lakshmanan <jawaharlakshmanan6@gmail.com>
2020-11-07 00:41:33 +0530

    initial python file

---
+++ test.py
@@ -0,0 +1,9 @@
+print("this a test to show that atom connected to github")
+safe = True
+print("don't know how to authorize with git")
+safed = input("Do i authorized safely : ")
+if safed == no:
+    safe = False
+while safe:
+    print("looks like atom worked")
+    break
